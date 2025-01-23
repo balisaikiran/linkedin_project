1b6f2e238b4ae25cb792f1965df8ecb5b3b4ef0a
Sai Kiran <sai@interface.re>
2025-01-23 18:07:30 +0530

    updates

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,12 @@ app.use(cors());
 app.use(express.json());
 
 // Here are the project parameters
-mongoose.connect('mongodb://localhost:27017/clicker-game');
+mongoose.connect('mongodb://localhost:27017/clicker-game', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('MongoDB connection error:', err));
 
 app.post('/api/click', async (req, res) => {
   try {
